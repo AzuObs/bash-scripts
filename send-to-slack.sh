50a6c9ffbdc6f3a878c38b2295258df51e383869
AzuObs <dleaverfr@gmail.com>
2017-01-09 19:32:20 +0100

    Fix send-to-slack

--- send-to-slack.sh
+++ send-to-slack.sh
@@ -2,10 +2,8 @@
 
 SERVICE=${1:?'First parameter must be a service'}
 
-if [[ -s /dev/stdin ]]; then
-	curl -v "https://hooks.slack.com/services/${SERVICE}" \
-	-H "Content-Type: application/json" \
-	-d @- << EOF
-		{"text": "Open pull requests\n $( cat /dev/stdin )"}
-	EOF
-fi
+TEXT=$( sed 's/$/\\n/g' /dev/stdin )
+
+curl -v "https://hooks.slack.com/services/${SERVICE}" \
+-H "Content-Type: application/json" \
+-d @- <<< "{\"text\": \"$TEXT\"}"
