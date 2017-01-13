62868c72dc31074ba1cc52fca9435bf434b90fe7
AzuObs <dleaverfr@gmail.com>
2017-01-13 15:51:35 +0100

    Edit remove-prefix-from-files to be able to rename any prefix

--- remove-prefix-from-files.sh
+++ remove-prefix-from-files.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 FOLDER=${1:?"First parameter must be the folder"}
+PREFIX=${2:?"Second parameter must be a prefix"}
 
 for FILE in $( ls $FOLDER )
 do
-	mv "$FOLDER/$FILE" "$FOLDER/${FILE##favicon@}"
+	mv "$FOLDER/$FILE" "$FOLDER/${FILE#$PREFIX}"
 done
 
 
