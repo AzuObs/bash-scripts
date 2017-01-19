bec8e5af52f0b78dfe4375ca79eadd59c98b945b
AzuObs <dleaverfr@gmail.com>
2017-01-20 00:41:05 +0100

    docker-rm reformat

--- docker-rm.sh
+++ docker-rm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 		#########################################################################
-		#	 	Remove Docker Entities - images, containers		                #
-		#									                                    #
-		#			dleaverfr@gmail.com				                            #
-		#			 						                                    #
-		#									                                    #
+		#	 	Remove Docker Entities - images, containers						#
+		#																		#
+		#			dleaverfr@gmail.com											#
+		#			 															#
+		#																		#
 		#########################################################################
 
 parse_positional_params(){
@@ -55,50 +55,50 @@ queue_docker_entities() {
 
 # prompt "Are you sure" if option -f wasn't set
 confirm_queue() {
-    if [[ -z ${SILENT+"expand_if_set"} ]]; then
-        printf "$RM_CONTAINERS$RM_IMAGES\n"
-        read -p "Are you sure you want to delete these docker instances? (Yn) " GO_AHEAD
+	if [[ -z ${SILENT+"expand_if_set"} ]]; then
+		printf "$RM_CONTAINERS$RM_IMAGES\n"
+		read -p "Are you sure you want to delete these docker instances? (Yn) " GO_AHEAD
 
-        if [[ -z $GO_AHEAD ]]; then
-            GO_AHEAD="Y"
-        fi
-    else
-        GO_AHEAD="Y"
-    fi
+		if [[ -z $GO_AHEAD ]]; then
+		GO_AHEAD="Y"
+		fi
+	else
+		GO_AHEAD="Y"
+	fi
 
-    if [[ ! $GO_AHEAD == "Y" ]]; then
-        printf "Exiting...\n"
-        exit 0
-    fi
+	if [[ ! $GO_AHEAD == "Y" ]]; then
+		printf "Exiting...\n"
+		exit 0
+	fi
 }
 
 # delete queue
 delete_queue() {
-    if [[ -n ${RM_CONTAINERS+"expand_if_set"} ]]; then
-        while IFS="" read -r CONTAINER; do
-            CONTAINER_ID=$( printf "$CONTAINER" | awk '{print $1}' )
-            printf "Deleting container $CONTAINER_ID...\n"
-            docker rm $CONTAINER_ID
-        done < <( printf "$RM_CONTAINERS\n" )
-    fi
+	if [[ -n ${RM_CONTAINERS+"expand_if_set"} ]]; then
+		while IFS="" read -r CONTAINER; do
+			CONTAINER_ID=$( printf "$CONTAINER" | awk '{print $1}' )
+		printf "Deleting container $CONTAINER_ID...\n"
+		docker rm $CONTAINER_ID
+		done < <( printf "$RM_CONTAINERS\n" )
+	fi
 
-    if [[ -n ${RM_IMAGES+"expand_if_set"} ]]; then
-        while IFS="" read -r IMAGE; do
-            IMAGE_ID=$( printf "$IMAGE" | awk '{print $3}' )
-            docker rmi $IMAGE_ID
+	if [[ -n ${RM_IMAGES+"expand_if_set"} ]]; then
+		while IFS="" read -r IMAGE; do
+			IMAGE_ID=$( printf "$IMAGE" | awk '{print $3}' )
+			docker rmi $IMAGE_ID
 
-            if (( $? >= 1 )) && [[ -z ${SILENT+"expand_if_set"} ]];then
-                # redirect input here because otherwise it reads from < <( printf "$RM_IMAGES" )
-                read -p "Force deleted $IMAGE_ID? (Yn) " FORCE_DELETE <&1
+			if (( $? >= 1 )) && [[ -z ${SILENT+"expand_if_set"} ]];then
+				# redirect input here because otherwise it reads from < <( printf "$RM_IMAGES" )
+				read -p "Force deleted $IMAGE_ID? (Yn) " FORCE_DELETE <&1
 
-                if [[ -n ${FORCE_DELETE+"expand_if_set"}} ]] && [[ $FORCE_DELETE == "Y" ]]; then
-                    docker rmi -f $IMAGE_ID
-                fi
-            fi
-        done < <( printf "$RM_IMAGES\n" )
-    fi
+				if [[ -n ${FORCE_DELETE+"expand_if_set"}} ]] && [[ $FORCE_DELETE == "Y" ]]; then
+					docker rmi -f $IMAGE_ID
+				fi
+			fi
+		done < <( printf "$RM_IMAGES\n" )
+	fi
 
-    printf "Done\n"
+	printf "Done\n"
 }
 
 # Start script execution
